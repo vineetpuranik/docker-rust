0647310aec13a41be6fccdae67e334cc3fb1568c
vnt.manu@gmailVineet Puranik <vnt.manu@gmail.com>
2024-02-17 22:39:14 -0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Stdio;
+
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -11,6 +13,8 @@ fn main() -> Result<()> {
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .output()
         .with_context(|| {
             format!(
@@ -21,7 +25,9 @@ fn main() -> Result<()> {
     
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{std_out}");
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        print!("{std_err}");
     } else {
         std::process::exit(1);
     }
