51923d545712ea4b3da5fe7760315238c13dfa71
Vineet Puranik <vnt.manu@gmail.com>
2024-03-23 10:53:47 -0700

    adding notes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,26 @@ use std::fs;
 
 
 //created connection between code crafters and my github repo
-//create an isolated path under a new folder called temp in the current directory
+//create an isolated path under a new folder called temp in the current directory.This will represent the sandboxed environment for our isolated execution.
+//create dev/null in the temp directory based on the instructions
+//create /usr/local/bin in the temp directory
+//read the arguments and copy the contents of the command binary supplied by the user to our temp directory
+//chroot in our temp directory to created an isolated environment
+//change the current working directory to the temp working directory
+//creating a new process using the input command and this will use the temp directory as the starting environment for the process
+//wire output and error from the new process to our parent
+//get the exit code of the new process and make sure the parent also exists with the same exit code.
+
+
+
+//Sandboxing process
+//Chroot
+//fs create directory
+//parse user arguments
+//copy contents of one file to the other file using fs copy
+//change working directory set_current_dir
+//spawn a child process and provide the command for the child process
+//wire up child process output and error to parent.
 const ISOLATED_PATH: &str = "./temp";
 
 // Usage: your_docker.sh run <image> <command> <arg> <arg2> ...
@@ -19,7 +38,7 @@ fn main() -> Result<()> {
     let create_dir_path = format!("{}{}", ISOLATED_PATH, "/dev/null");
     fs::create_dir_all(create_dir_path)?;
     
-    // create the /usr/local/bin directory in our temo directory
+    // create the /usr/local/bin directory in our temp directory
     let create_bin_directory = format!("{}{}", ISOLATED_PATH, "/usr/local/bin/");
     fs::create_dir_all(create_bin_directory)?;
     
@@ -28,7 +47,7 @@ fn main() -> Result<()> {
     let command = &args[3];
     let command_args = &args[4..];
 
-    //copy binary to current working directory
+    //copy the contents of the binary provided by the user to our current working directory
     fs::copy(command, format!("{}{}", ISOLATED_PATH, command)).context("Failed to copy")?;
 
     //perform chroot operation
